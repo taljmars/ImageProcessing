7dbef01b92fdf77962f76b60faf6a351be636513
taljmars <taljmars@gmail.com>
2017-03-06 04:16:25 +0200

    maven support

--- src/ObjectsDetector/ObjectDetectorListener.java
+++
@@ -1,8 +0,0 @@
-package ObjectsDetector;
-
-import ObjectsDetector.Utilities.DetectionResults;
-
-public interface ObjectDetectorListener {
-	
-	public void handleImageProcessResults(DetectionResults frameProcessResult);
-}
